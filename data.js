a8f6d3cfe808ed26eb51790b144beaada6afe2d4
dafsgzassdasda-png <dafsgzassdasda@gmail.com>
2026-04-06 14:44:30 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,355 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-banheiro-feminino",
+      "name": "banheiro feminino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45730834889217853,
+          "pitch": 0.8178192774620694,
+          "rotation": 0.7853981633974483,
+          "target": "2-centro-da-igreja"
+        },
+        {
+          "yaw": -1.2509684293454573,
+          "pitch": 1.2115890899993165,
+          "rotation": 0,
+          "target": "4-parte-de-tras-igreja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-banheiro-masculino",
+      "name": "banheiro masculino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4938766172101001,
+          "pitch": 0.7610260876607473,
+          "rotation": 0.7853981633974483,
+          "target": "2-centro-da-igreja"
+        },
+        {
+          "yaw": -0.14247517686774103,
+          "pitch": 1.372245717540748,
+          "rotation": 0,
+          "target": "4-parte-de-tras-igreja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-centro-da-igreja",
+      "name": "centro da igreja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6560796823533117,
+          "pitch": 0.5296174615386828,
+          "rotation": 0,
+          "target": "4-parte-de-tras-igreja"
+        },
+        {
+          "yaw": -1.2237043985992635,
+          "pitch": 0.5784837933982967,
+          "rotation": 1.5707963267948966,
+          "target": "5-pergolado-fechado"
+        },
+        {
+          "yaw": -2.1230819348841035,
+          "pitch": 0.5971244811362464,
+          "rotation": 4.71238898038469,
+          "target": "3-em-baixo-do-pergolado"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-em-baixo-do-pergolado",
+      "name": "em baixo do pergolado",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.043487618583293,
+          "pitch": 0.4793308581049409,
+          "rotation": 0,
+          "target": "4-parte-de-tras-igreja"
+        },
+        {
+          "yaw": -3.0734164163999864,
+          "pitch": 0.8061535451915933,
+          "rotation": 0.7853981633974483,
+          "target": "4-parte-de-tras-igreja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-parte-de-tras-igreja",
+      "name": "parte de tras igreja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0572997460878497,
+          "pitch": 0.5491868268000601,
+          "rotation": 0,
+          "target": "2-centro-da-igreja"
+        },
+        {
+          "yaw": -2.563408090596278,
+          "pitch": 0.6312112816009066,
+          "rotation": 0.7853981633974483,
+          "target": "3-em-baixo-do-pergolado"
+        },
+        {
+          "yaw": 2.6981916653440656,
+          "pitch": 0.6618704714859263,
+          "rotation": 5.497787143782138,
+          "target": "5-pergolado-fechado"
+        },
+        {
+          "yaw": -1.2640670172193413,
+          "pitch": 0.6454681756154592,
+          "rotation": 0,
+          "target": "1-banheiro-masculino"
+        },
+        {
+          "yaw": 1.4094439343365117,
+          "pitch": 0.8106756569878186,
+          "rotation": 0,
+          "target": "0-banheiro-feminino"
+        },
+        {
+          "yaw": 0.010286505721925465,
+          "pitch": 0.6506232812979089,
+          "rotation": 0,
+          "target": "6-porta-de-entrada-da-igreja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pergolado-fechado",
+      "name": "pergolado fechado",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4527589291774543,
+          "pitch": 0.6513347747272107,
+          "rotation": 0,
+          "target": "2-centro-da-igreja"
+        },
+        {
+          "yaw": -0.34445289996747164,
+          "pitch": 0.9997665908616806,
+          "rotation": 5.497787143782138,
+          "target": "4-parte-de-tras-igreja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-porta-de-entrada-da-igreja",
+      "name": "porta de entrada da igreja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6083620946685668,
+          "pitch": 0.25582286049447767,
+          "rotation": 0,
+          "target": "4-parte-de-tras-igreja"
+        },
+        {
+          "yaw": 2.556916867546617,
+          "pitch": 0.11069316014457087,
+          "rotation": 0.7853981633974483,
+          "target": "1-banheiro-masculino"
+        },
+        {
+          "yaw": 1.007817213092375,
+          "pitch": 0.18520049946653572,
+          "rotation": 5.497787143782138,
+          "target": "0-banheiro-feminino"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "igrejan nova",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
